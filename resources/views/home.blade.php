e5612f15a67422ee286e9fa0253057333637b403
Jan <asaulenkojan@gmail.com>
2018-04-22 19:01:47 +0300

    moders add

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,51 +25,95 @@
             <div class="col-sm"><a href="#">View all</a></div>
         </div>
     </div>
+
+
+
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-5">
                 <div class="row">
-                    <div class="col-sm-6"><h2>Коментарий1</h2></div>
+                    <div class="col-sm-6"><h3>Коментарий1</h3></div>
                     <div class="col-sm-6">Date</div>
                 </div>
                 <div class="row">
-                    <div class="col-sm">USER:An accaunt is required in order to save your project and access sharing</div>
+                    <div class="col-sm">USER:An accaunt is required in order to save your project and access sharing
+                    </div>
                 </div>
             </div>
-            <div class="col-sm-2"> <img src="{{asset('images/home/images.png')}}"></div>
-            <div class="col-sm-5"><a href="#">View all</a></div>
+            <div class="col-sm-2"><h3>My Maps </h3>
+                    <img src="http://via.placeholder.com/150x150" alt="">
+            </div>
+            <div class="col-sm-5"><h3>Photo </h3>
+                    <img src="{{asset('images/home/images.png')}}"></div>
         </div>
         <div class="row">
             <div class="col-sm-5">
-                <h2>Коментарий2</h2>
-
+                <div class="row">
+                    <div class="col-sm-6"><h3>Коментарий2</h3></div>
+                    <div class="col-sm-6">Date</div>
+                </div>
+                <div class="row">
+                    <div class="col-sm">USER:An accaunt is required in order to save your project and access sharing
+                    </div>
+                </div>
             </div>
-            <div class="col-sm-2"><a href="#">View all</a></div>
-            <div class="col-sm-5"><a href="#">View all</a></div>
+            <div class="col-sm-2">
+                <img src="http://via.placeholder.com/150x150" alt="">
+            </div>
+            <div class="col-sm-5">
+                <img src="{{asset('images/home/images.png')}}"></div>
         </div>
         <div class="row">
             <div class="col-sm-5">
-                <h2>Коментарий3</h2>
-
+                <div class="row">
+                    <div class="col-sm-6"><h3>Коментарий3</h3></div>
+                    <div class="col-sm-6">Date</div>
+                </div>
+                <div class="row">
+                    <div class="col-sm">USER:An accaunt is required in order to save your project and access sharing
+                    </div>
+                </div>
             </div>
-            <div class="col-sm-2"><a href="#">View all</a></div>
-            <div class="col-sm-5"><a href="#">View all</a></div>
+            <div class="col-sm-2">
+                <img src="http://via.placeholder.com/150x150" alt="">
+            </div>
+            <div class="col-sm-5">
+                <img src="{{asset('images/home/images.png')}}"></div>
         </div>
         <div class="row">
             <div class="col-sm-5">
-                <h2>Коментарий4</h2>
-
+                <div class="row">
+                    <div class="col-sm-6"><h3>Коментарий4</h3></div>
+                    <div class="col-sm-6">Date</div>
+                </div>
+                <div class="row">
+                    <div class="col-sm">USER:An accaunt is required in order to save your project and access sharing
+                    </div>
+                </div>
             </div>
-            <div class="col-sm-2"><a href="#">View all</a></div>
-            <div class="col-sm-5"><a href="#">View all</a></div>
+            <div class="col-sm-2">
+                <img src="http://via.placeholder.com/150x150" alt="">
+            </div>
+            <div class="col-sm-5">
+                <img src="{{asset('images/home/images.png')}}"></div>
         </div>
         <div class="row">
             <div class="col-sm-5">
-                <h2>Коментарий5</h2>
-
+                <div class="row">
+                    <div class="col-sm-6"><h3>Коментарий5</h3></div>
+                    <div class="col-sm-6">Date</div>
+                </div>
+                <div class="row">
+                    <div class="col-sm">USER:An accaunt is required in order to save your project and access sharing
+                    </div>
+                </div>
             </div>
-            <div class="col-sm-2"><a href="#">View all</a></div>
-            <div class="col-sm-5"><a href="#">View all</a></div>
+            <div class="col-sm-2">
+                <img src="http://via.placeholder.com/150x150" alt="">
+            </div>
+            <div class="col-sm-5">
+                <img src="{{asset('images/home/images.png')}}"></div>
         </div>
+
     </div>
 @endsection
