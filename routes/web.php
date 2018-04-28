6939a2791834e9f4b140ab423ea4856a873d05a5
unknown <mamayserg1@gmail.com>
2018-04-28 19:03:15 +0300

    delete rout wiew all

--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,15 @@ Route::resource('/posts','PostController')->only([
 'index','store', 'create']);
 //Route::get ('/request-form','PostController@show');
 
-Route::resource('moders','ModeratorFormController')->only([
-        'index','show','edit']
-);
-<<<<<<< HEAD
+
+
 
 Route::resource('complains','ComplainsController')->only([
 'index','show']
 );
 
-Route::resource('/view_all','ViewAllController')->only([
-    'index']);
-
-;
 
 
-=======
 Route::resource('/admin','AdminController')->only([
     'index']);
->>>>>>> 07982e1c858cde033f53b866152f2115a42d002c
+
