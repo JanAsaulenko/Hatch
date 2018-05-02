d9b90665aa314161723d2c8e999fbd87a40a7bf5
Sergiy Kotsur <sergesk17@gmail.com>
2018-05-02 20:35:38 +0300

    Serge

--- resources/views/moders/complains/edit.blade.php
+++ resources/views/moders/complains/edit.blade.php
@@ -11,49 +11,51 @@
 
     <h1>Сторінка редагування скарга № {{ $complain->id }}</h1>
 
-    <table style="width: 100%"border="1">
-    <tr>
-        <th scope="col">Дата</th>
-        <th scope="col">Заголовок скарги </th>
-        <th scope="col">Ім'я користувача</th>
-        <th scope="col">Текст скарги</th>
-        <th scope="col">Затвердити </th>
-        <th scope="col">Повернутись до списку</th>
-    </tr>
-    <tr>
-        <form action="" method="post">
-            <input type="hidden" name="id" value="{{$complain->id}}">
-            <td><input type="date" name="created_at" value="{{$complain->created_at}}"></td>
-            <td><input type="text" name="title" value="{{$complain->title}}"></td>
-            <td><input type="text" name="username" value="{{$complain->username}}"></td>
-            <td><textarea rows="7" cols="50" id="comments" name="comments">{{$complain->comments}}</textarea></td>
-            <td><input type="submit" name="seve" value="Зберегти"></td>
-            <td>
-                <a href="{{ url('moders/' ) }}" type="link" class="btn btn-outline-success btn-sm" >До списку скарг</a>
-            </td>
+    {{ Form::open(array('url' => 'moders/complains/'. $complain->id, 'method' => 'PUT')) }}
 
-        </form>
-    </tr>
-    </table>
-    <?php
-//    print_r($_GET);
-    ?>
+        <div class="row">
+
+        <div class="col-md-2" class="form-group">
+                {{ Form::label('created_at', "Дата створення", ['class' => 'control-label']) }}
+                {{ Form::date('created_at', $complain->created_at, [
+                    'class' => $errors->has('created_at') ? 'has-error' : "",
+                ]) }}
+            </div>
+            <div class="col-md-2" class="form-group">
+                {{ Form::label('tittle', "Заголовок скарги", ['class' => 'control-label']) }}
+                {{ Form::text('tittle', $complain->tittle, [
+                    'class' => $errors->has('tittle') ? 'has-error' : "",
+                ]) }}
+            </div>
+            <div class="col-md-2" class="form-group">
+                {{ Form::label('username', "Ім'я користувача", ['class' => 'control-label']) }}
+                {{ Form::text('username', $complain->username, [
+                    'class' => $errors->has('username') ? 'has-error' : "",
+                ]) }}
+            </div>
+        <div class="col-md-5" class="form-group">
+                {{ Form::label('comments', "Текст скарги", ['class' => 'control-label']) }}
+                {{ Form::textarea('comments', $complain->comments, [
+                    'row' => '5', 'class' => 'form-control'
+                ]) }}
+            </div>
+
+        <div class="col-md-1" class="form-group">
+
+            {{ Form::submit('Зберегти', array('class' => 'btn btn-primary')) }}
+
+            <a href="{{ url('moders/complains/' ) }}" type="link" class="btn btn-outline-success btn-sm" >До списку скарг</a>
+        </div>
+
+    {{ Form::close() }}
 
 
-    <ul class="pagination justify-content-center">
-        <li class="page-item">
-            <a class="page-link" href="#" aria-label="Previous">
-                <span aria-hidden="true">&laquo;</span>
-                <span class="sr-only">Previous</span>
-            </a>
-        </li>
-        <li class="page-item">
-            <a class="page-link" href="#" aria-label="Next">
-                <span aria-hidden="true">&raquo;</span>
-                <span class="sr-only">Next</span>
-            </a>
-        </li>
-    </ul>
+
+    </div>
+
+
+
+
 
 
 @endsection
--- resources/views/moders/complains/update.blade.php
+++
@@ -1,9 +0,0 @@
-<?php
-$tdid=$_POST['id'];
-$tdnews=$_POST['news'];
-if ( isset ( $_POST['submit'] ) )
-{
-    echo '<input type="text" name="idup" value='.$tdid.'>';
-    echo '<input type="text" name="textup" value='.$tdnews.'>';
-}
-?>
