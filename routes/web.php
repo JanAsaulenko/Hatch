b06c8d3501795e9e3d7dbabf96a7e59fd6ab9014
dukovw <vladdukov86@gmail.com>
2018-05-13 13:22:35 +0300

    merge

--- routes/web.php
+++ routes/web.php
@@ -18,25 +18,9 @@ Route::resource('/posts','PostController')->only([
 //Route::get ('/request-form','PostController@show');
 
 
-
-
-
-
-
-
     Route::resource('/moders/complains', 'Moders\ComplainsController')->only([
         'index','edit','update','show'
     ]);
-
-
-
-
-
-
-
-
-
-
 
 Route::resource('/admin','AdminController')->only([
     'index']);
