b461c2a281ec9b3ea218542a8872b028dc4843c2
Sergey Lysov <serglysov@gmail.com>
2018-05-26 13:31:19 +0300

    made bootstrap css for moders index, insert 'if' to columns

--- resources/views/moders/complains/index.blade.php
+++ resources/views/moders/complains/index.blade.php
@@ -6,90 +6,105 @@
 <html lang="en">
 
 
-@section('title', 'Form');
+@section('title', 'Form')
 
 
 
 @section('content')
 
-
-
     <!-- Main content -->
+    <div class="row">
+        <!-- header -->
+        <div class=" col-md-2 col-sm-3 col-xs-12 tbtxt"><h5>Заголовок скарги</h5></div>
+        <div class=" col-md-3 col-sm-6 col-xs-12 tbtxt "><h5>Текст скарги</h5></div>
+        <div class=" col-md-1 col-sm-3 col-xs-4 tbtxt "><h5>Ім'я </h5></div>
+        <div class=" col-md-1 col-sm-2 col-xs-4 tbtxt"><h5>Стан </h5></div>
+        <div class=" col-md-1 col-sm-2 col-xs-4 tbtxt"><h5>Фото </h5></div>
+        <div class=" col-md-1 col-sm-2 col-xs-3 tbtxt"></div>
+        <div class=" col-md-1 col-sm-2 col-xs-3 tbtxt"></div>
+        <div class=" col-md-1 col-sm-2 col-xs-3 tbtxt"></div>
+        <div class=" col-md-1 col-sm-2 col-xs-3 tbtxt"></div>
 
-         <a href="{{ url('moders/complains/' ) }}" type="link" class="btn btn-outline-success btn-sm" >До списку всіх скарг</a>
-        <a href="?confirmed=1" type="link" class="btn btn-outline-success btn-sm" >До списку незатверджених скарг</a>
-        <table class="table table-hover">
-            <thead>
-            <tr>
-                <th scope="col" class="tbtxt">Заголовок скарги</th>
-                <th scope="col" class="tbtxt">Текст скарги</th>
-                <th scope="col" class="tbtxt">Ім'я скаржника </th>
-                <th scope="col" class="tbtxt">Стан затвердження </th>
-                <th scope="col" class="tbtxt">Наявність фото </th>
-                <th scope="col" class="tbtxt"> </th>
-                <th scope="col" class="tbtxt"> </th>
-                <th scope="col" class="tbtxt"> </th>
-                <th scope="col" class="tbtxt"> </th>
+        <!-- loop -->
+        @foreach($complains as $complain)
 
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($complains as $complain)
+            <div class=" col-md-2 col-sm-3 col-xs-12 tdtxt">
+                <div>{{$complain->title}}</div>
+            </div>
+            <div class=" col-md-3 col-sm-6 col-xs-12 tdtxt ">
+                <div>{{$complain->comments}}</div>
+            </div>
+            <div class=" col-md-1 col-sm-3 col-xs-4 tdtxt ">
+                <div>{{$complain->username}}</div>
+            </div>
+            <div class=" col-md-1 col-sm-2 col-xs-4 tdtxt">
 
-                <tr>
-                    <td class="tdtxt">
-                        <div>{{$complain->title}}</div>
-                    </td>
-                    <td class="tdtxt">
-                        <div>{{$complain->comments}}</div>
-                    </td>
-                    <td class="tdtxt">
-                        <div>{{$complain->username}}</div>
-                    </td>
-                    <td class="tdtxt">
+                @if ($complain->confirmed == 1)
+                    <i>Затверджена</i>
 
-                        <div>{{($complain->confirmed == 1) ? "Затверджена" : "НE затверджена!" }}</div>
-                    </td>
-                    <td scope="col" class="tdtxt" >
-                        <div >{{($complain->img) ? "Фото присутнє" : "Фото відсутнє"}}</div>
-                    </td>
-                    <td class="tdtxt">
-                        <form action="{{ url('moders/complains/'. $complain->id ) }}" method="POST">
-                            {{ csrf_field() }}
-                            {{method_field('PUT')}}
+                @else
+                    <del>Не затверджена</del>
+                @endif
+            </div>
+            <div class=" col-md-1 col-sm-2 col-xs-4 tdtxt">
+                @if ($complain->img)
+                    <i>Присутнє</i>
 
-                            <button type="submit" name="confirmed" class="btn btn-outline-success btn-sm" >
-                                <i class="far fa-save"></i>
-                            </button>
-                        </form>
-                    </td>
-                    <td class="tdtxt">
-                        <a href="{{ url('moders/complains/' . $complain->id . '/edit') }}" type="link" class="btn btn-outline-success btn-sm" >
-                            <i class="far fa-edit"></i>
-                        </a>
-                    </td>
-                    <td class="tdtxt">
-                        <a href="{{ url('moders/complains/' . $complain->id ) }}" type="link" class="btn btn-outline-success btn-sm" >
-                            <i class="fas fa-eye"></i>
-                        </a>
-                    </td>
-                    <td class="tdtxt">
+                @else
+                    <del>Відсутнє</del>
+                @endif
+            </div>
+            <div class=" col-md-1 col-sm-2 col-xs-3 tdtxt">
+                <form action="{{ url('moders/complains/'. $complain->id ) }}" method="POST">
+                    {{ csrf_field() }}
+                    {{method_field('PUT')}}
 
-                        <form action="{{url('moders/complains/' . $complain->id)}}" method="POST">
-                            {{csrf_field()}}
-                            {{method_field('DELETE')}}
-                            <button class="btn btn-outline-danger btn-sm">
-                                <i class="far fa-trash-alt"></i>
-                            </button>
-                        </form>
-                    </td>
+                    <button type="submit" name="confirmed" class="btn btn-outline-success btn-sm" >
+                        <i class="far fa-save"></i>
+                    </button>
+                </form>
+            </div>
+            <div class=" col-md-1 col-sm-2 col-xs-3 tdtxt">
+                <a href="{{ url('moders/complains/' . $complain->id . '/edit') }}" type="link" class="btn btn-outline-success btn-sm" >
+                    <i class="far fa-edit"></i>
+                </a>
+            </div>
+            <div class=" col-md-1 col-sm-2 col-xs-3 tdtxt">
+                <a href="{{ url('moders/complains/' . $complain->id ) }}" type="link" class="btn btn-outline-success btn-sm" >
+                    <i class="fas fa-eye"></i>
+                </a>
+            </div>
+            <div class=" col-md-1 col-sm-2 col-xs-3 tdtxt ">
+                <form action="{{url('moders/complains/' . $complain->id)}}" method="POST">
+                    {{csrf_field()}}
+                    {{method_field('DELETE')}}
+                    <button class="btn btn-outline-danger btn-sm">
+                        <i class="far fa-trash-alt"></i>
+                    </button>
+                </form>
+            </div>
 
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-        {{ $complains->links( "pagination::bootstrap-4") }}
+        @endforeach
+    <!-- bottom -->
+
+        <div class=" col-md-2 col-sm-3 col-xs-4 tbtxt">
+
+                <a href="{{ url('moders/complains/' ) }}" type="link" class="btn btn-outline-success btn-sm abtn" >Всі скарги</a>
+
+        </div>
+        <div class=" col-md-2 col-sm-3 col-xs-4 tbtxt ">
+            <a href="?confirmed=1" type="link" class="btn btn-outline-success btn-sm abtn" >Незатверджені скарги</a>
+        </div>
+        <div class=" col-md-6 col-sm-3 col-xs-1 tbtxt "></div>
+        <div class=" col-md-2 col-sm-3 col-xs-4 tbtxt">
+           <div class="abtn"> {{ $complains->links( ) }}</div>
+        </div>
+
     </div>
+
+
+
+
 
 
 
