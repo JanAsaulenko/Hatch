25687eeaf1ed73fc59e8c38c2d8a751ab21d75d6
Jan <asaulenkojan@gmail.com>
2018-06-30 21:37:55 +0300

    agreewithchanges

--- resources/views/districts/district_page.blade.php
+++ resources/views/districts/district_page.blade.php
@@ -2,6 +2,25 @@
 
 @section('district')
 
+  {{--<div class="position-block">--}}
+    {{--<div id="map_interactive"></div>--}}
+  {{--</div>--}}
+  {{--<div class="container-fluid">--}}
+    {{--<div class="row">--}}
+      {{--<div class="col-sm-8">--}}
+      {{--</div>--}}
+      {{--<div class="col-sm">--}}
+        {{--class="nav-item {{ (request()->is('posts/create') ? 'active' : '') }}">--}}
+        {{--<a class="nav-link" href="{{ url('/posts/create') }}"><h6><b>СТВОРИТИ СКАРГУ</b></h6></a>--}}
+        {{--<div class="row" id="fiveLatest">--}}
+          {{--@foreach($posts as $post)--}}
+            {{--<div class="col-md-6">--}}
+              {{--@include('post._card', ['post' => $post])--}}
+            {{--</div>--}}
+          {{--@endforeach--}}
+        {{--</div>--}}
 
-
-  @endsection
+      {{--</div>--}}
+    {{--</div>--}}
+  {{--</div>--}}
+@endsection
